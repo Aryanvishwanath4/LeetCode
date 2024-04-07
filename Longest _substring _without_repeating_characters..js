d51f2b73f4575e8faf141de61e1eab0162838766
Aryanvishwanath4 <aryanvaryan16@gmail.com>
2024-04-07 12:21:40 +0530

    lengthOfLongestSubstring

---
+++ Longest _substring _without_repeating_characters..js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let itr = 0;
+    let max = 0;
+    let str = []
+    for(let i =0 ;i< s.length;i++){
+        //enters loop and slices the first elements till that element has been removed from the str array
+        while(str.includes(s[i])){
+            str.splice(0,1)
+            itr++;
+        }
+        str.push(s[i])
+        max = Math.max(max, i - itr + 1)
+    }
+    return max;
+};
